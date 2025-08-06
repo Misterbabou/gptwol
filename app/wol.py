ca31a7c88e5cf530cbbe06e0e73ce4066856d1b4
Marco <marco_schwan@mailbox.org>
2025-08-06 16:01:11 +0200

    Update wol.py

--- app/wol.py
+++ app/wol.py
@@ -1,17 +1,21 @@
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from scapy.all import Ether, sendp
 import socket
 import struct
 import subprocess
 import os
 import ipaddress
 import re
+import binascii
 
 port = os.environ.get('PORT', 5000)
 ping_timeout = os.environ.get('PING_TIMEOUT', 300)
 arp_timeout = os.environ.get('ARP_TIMEOUT', 300)
 tcp_timeout = os.environ.get('TCP_TIMEOUT', 1)
 arp_interface = os.environ.get('ARP_INTERFACE')
+l2_wol_paket = os.environ.get('L2_WOL_PAKET')
+l2_interface = os.environ.get('L2_INTERFACE', 'eth0')
 cron_filename = '/etc/cron.d/gptwol'
 computer_filename = 'db/computers.txt'
 computer_old_filename = 'computers.txt'
@@ -124,6 +128,16 @@ def load_computers():
   return computers
 
 computers = load_computers()
+
+def send_raw_wol(mac_address, interface='eth0'):
+    mac_clean = mac_address.replace(':', '').replace('-', '')
+    mac_bytes = binascii.unhexlify(mac_clean)
+
+    magic_packet = b'\xff' * 6 + mac_bytes * 16
+
+    ether_frame = Ether(dst='ff:ff:ff:ff:ff:ff', type=0x0842) / magic_packet
+
+    sendp(ether_frame, iface=interface, verbose=False)
 
 def send_wol_packet(mac_address):
   # Convert the MAC address to a packed binary string
@@ -411,7 +425,10 @@ def wol_or_sol_send():
     messages.append(f"Sleep On Lan Magic Packet Sent to {computer['name']}!")
     messages.append('See : <a href="https://github.com/Misterbabou/gptwol#configure-sleep-on-lan" target="_blank" rel="noopener noreferrer">how to configure Sleep on LAN</a>')
   else:
-    send_wol_packet(mac_address)
+    if l2_wol_paket:
+      send_raw_wol(mac_address, l2_interface)
+    else:
+      send_wol_packet(mac_address)
     title = "Wakeup"
     messages.append(f"Wake On Lan Magic Packet Sent to {computer['name']}!")
 
